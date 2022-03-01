835b7f62457712ed6f9b0d9650fa65b848e5e177
ASUS <h_khobani@yahoo.com>
2022-03-01 23:47:49 +0330

    list story end

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:blog_club/data.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 import 'common/theme.dart';
@@ -26,7 +27,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var list = AppDatabase.stories;
+    List<StoryData> list = AppDatabase.stories;
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
@@ -67,66 +68,104 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _list_stories({required list}) {
-   return Container(
+    return Container(
       height: 100,
       margin: const EdgeInsets.only(top: 16),
       //color: Colors.grey,
       child: ListView.builder(
+        physics: BouncingScrollPhysics(),
           itemCount: list.length,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
-            var item = list[index];
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 1, horizontal: 4),
-                      width: 70,
-                      height: 70,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(24),
-                          gradient: LinearGradient(
-                            colors: [
-                              Colors.blue.shade800,
-                              Colors.blue.shade400,
-                              Colors.blue.shade100,
-                            ],
-                            begin: Alignment.topCenter,
+            StoryData item = list[index];
+            return Container(
+              margin: EdgeInsets.only(top: 2,bottom: 2),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Stack(
+                    children: [
+                      item.isViewed ?
+                      _profile_view_viewed(imageFileName: item.imageFileName,context: context):
+                      _profile_view_normal(imageFileName: item.imageFileName),
+                      Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: Image.asset(
+                            'assets/img/icons/${item.iconFileName}',
+                            width: 25,
                           )),
-                      child: Container(
-                        padding: EdgeInsets.all(4),
-                        margin: EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                        ),
-                        child: ClipRRect(
-                            clipBehavior: Clip.antiAlias,
-                            borderRadius: BorderRadius.circular(18),
-                            child: Image.asset(
-                                'assets/img/stories/${item.imageFileName}')),
-                      ),
-                    ),
-                    Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: Image.asset('assets/img/icons/${item.iconFileName}',width: 25,)),
-                  ],
-                ),
-                Text(
-                  item.name,
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle1!
-                      .copyWith(
-                      fontWeight: FontWeight.bold, fontSize: 13),
-                ),
-              ],
+                    ],
+                  ),
+                  Text(
+                    item.name,
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle1!
+                        .copyWith(fontWeight: FontWeight.bold, fontSize: 13),
+                  ),
+                ],
+              ),
             );
           }),
+    );
+  }
+
+  Widget _profile_widget({required String imageFileName}) {
+    return ClipRRect(
+      clipBehavior: Clip.antiAlias,
+      borderRadius: BorderRadius.circular(18),
+      child: Image.asset('assets/img/stories/${imageFileName}'),
+    );
+  }
+
+  Widget _profile_view_normal({required String imageFileName}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+          vertical: 1, horizontal: 4),
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24),
+          gradient: LinearGradient(
+            colors: [
+              Colors.blue.shade800,
+              Colors.blue.shade400,
+              Colors.blue.shade100,
+            ],
+            begin: Alignment.topCenter,
+          )),
+      child: Container(
+        padding: EdgeInsets.all(4),
+        margin: EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+        ),
+        child:
+        _profile_widget(imageFileName: imageFileName),
+      ),
+    );
+  }
+  Widget _profile_view_viewed({required String imageFileName,required BuildContext context}) {
+    return SizedBox(
+      width: 70,
+      height: 70,
+      child: DottedBorder(
+        radius: Radius.circular(24),
+        strokeWidth: 2.5,
+        color: Theme.of(context).textTheme.bodyText2!.color!,
+        borderType: BorderType.RRect,
+        dashPattern: [5,3],
+        padding: EdgeInsets.all(6),
+        child: Container(
+
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+          ),
+          child: _profile_widget(imageFileName: imageFileName),
+        ),
+      ),
     );
   }
 }
